414dab85be5cfc47bbbcb272d91f40d15c80a218
Jay Tumarao <Jay.Tumarao@gmail.com>
2020-08-03 17:54:54 -0700

    Review SumIntegersByWeight

--- src/SumIntegersByWeight/SumIntegersByWeight.js
+++ src/SumIntegersByWeight/SumIntegersByWeight.js
@@ -19,8 +19,26 @@ function sumNestedIntegers(myList, depth = 1) {
 }
 
 
+/**
+ * Given nested list of integers, return sum
+ * of all integers in the list weighted by 
+ * their depth.
+ */
+function sumNestedIntegers2(data, depth = 1) {
+    let result = 0;
+    for(let x=0;x<data.length;x++) {
+        if (Array.isArray(data[x])) {
+            result += sumNestedIntegers2(data[x], depth + 1);
+        } else if (Number.isInteger(data[x])) {
+            result += data[x] * depth;
+        }
+    }
+    return result;
+}
 
-const result = sumNestedIntegers([
+
+
+const data = [
     [
         1
         ,1
@@ -34,5 +52,8 @@ const result = sumNestedIntegers([
         ]
         ,1
     ]
-]);
-console.log(result);
+];
+const result = sumNestedIntegers(data);
+const result2 = sumNestedIntegers2(data);
+console.log(result == result2, ': Answers are the same');
+console.log(result == 425, ': Answer is correct');
